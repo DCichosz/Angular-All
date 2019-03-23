7d18a2ca92597a22b2da47b8319fdf26125ce976
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-23 20:35:52 +0100

    ngModel and name

--- Zadania Angular 7 Kurs Udemy/forms-td-start/src/app/app.component.ts
+++ Zadania Angular 7 Kurs Udemy/forms-td-start/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  suggestedName = '';
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    this.suggestedName = 'Superuser';
   }
 }
