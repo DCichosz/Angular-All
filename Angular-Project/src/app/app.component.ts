09eefae8855272fcfb0bcb0f9722a666858253d2
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-23 20:04:56 +0100

    refactor

--- Angular-Project/src/app/app.component.ts
+++ Angular-Project/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RecipeService } from './recipes/recipe.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent { }
