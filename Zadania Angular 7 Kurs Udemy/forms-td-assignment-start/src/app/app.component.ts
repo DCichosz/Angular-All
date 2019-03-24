214958f7eb4ddb13a606a6dfb6485f636b6847d9
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-24 21:40:01 +0100

    form exercise

---
+++ Zadania Angular 7 Kurs Udemy/forms-td-assignment-start/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = "Advanced";
+
+  @ViewChild('form') form: NgForm;
+
+  onSubmit(): void {
+    console.log(this.form);
+  }
+}
