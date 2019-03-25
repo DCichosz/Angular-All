69797492ab8193d8b86f9d3ea82427fdf6f31744
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-25 15:53:18 +0100

    custom pipe creation

---
+++ Zadania Angular 7 Kurs Udemy/pipes-start/src/app/shorten.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform {
+
+  transform(value: any) {
+    if (value.length > 10) {
+      return `${value.substr(0, 10)} ...`;
+    } else {
+      return value;
+    }
+
+  }
+}
