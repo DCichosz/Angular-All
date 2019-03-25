5c1c13ef6811a3d3c263923ef26459f6378d3b2c
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-25 17:51:01 +0100

    fix it

--- Zadania Angular 7 Kurs Udemy/http-start/src/app/server.service.ts
+++ Zadania Angular 7 Kurs Udemy/http-start/src/app/server.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class ServerService {
@@ -7,15 +8,19 @@ export class ServerService {
 
   storeServers(servers: any[]) {
     // można przesyłać właśne headery
-    const headers = new Headers({'Content-Type': 'application/json'});
+    const headers = new Headers({ 'Content-Type': 'application/json' });
     // zwróci to tylko observable -> zeby wyslac polecenie musimy zasubskrybować
-     return this.httpService.post('https://udemy-ng-http-92fec.firebaseio.com/data.json', servers, {headers: headers});
+    return this.httpService.post('https://udemy-ng-http-92fec.firebaseio.com/data.json', servers, { headers: headers });
 
 
-   // return this.httpService.put('https://udemy-ng-http-92fec.firebaseio.com/data.json', servers, {headers: headers});
+    // return this.httpService.put('https://udemy-ng-http-92fec.firebaseio.com/data.json', servers, {headers: headers});
   }
 
   getServers() {
-    return this.httpService.get('https://udemy-ng-http-92fec.firebaseio.com/data.json');
+    return this.httpService.get('https://udemy-ng-http-92fec.firebaseio.com/data.json')
+      .pipe(map((response: Response) => {
+        const data = response.json();
+        return data;
+      }));
   }
 }
