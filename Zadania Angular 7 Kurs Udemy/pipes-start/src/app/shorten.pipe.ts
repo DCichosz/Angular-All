ab3e75f138372a3ae8c3c9b1a7501e07ae58bcca
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-25 15:54:51 +0100

    added arguments to custom pipe

--- Zadania Angular 7 Kurs Udemy/pipes-start/src/app/shorten.pipe.ts
+++ Zadania Angular 7 Kurs Udemy/pipes-start/src/app/shorten.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
 
-  transform(value: any) {
-    if (value.length > 10) {
-      return `${value.substr(0, 10)} ...`;
+  transform(value: any, limit: number) {
+    if (value.length > limit) {
+      return `${value.substr(0, limit)} ...`;
     } else {
       return value;
     }
