967784f916da1360a4c4224c77d3b746a3132d6b
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-25 14:12:07 +0100

    add/edit recipe logic - Reactive forms aproach, custom validation

--- Angular-Project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Angular-Project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
 	selector: 'app-recipe-edit',
@@ -27,16 +28,28 @@ export class RecipeEditComponent implements OnInit {
 	}
 
 	onSubmit(): void {
-		console.log(this.recipeForm);
+		// const newRecipe = new Recipe(this.recipeForm.value['name'],
+		// 	this.recipeForm.value['description'],
+		// 	this.recipeForm.value['imagePath'],
+		// 	this.recipeForm.value['ingredients']);
+		// zamiast uzywac tego wyzej w parametrze metody
+		// mozna uzyc this.recipeForm.value
+
+
+		if (this.editMode) {
+			this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+		} else {
+			this.recipeService.addRecipe(this.recipeForm.value);
+		}
 	}
 
 	onAddIngredient() {
 		(<FormArray>this.recipeForm.get('ingredients')).push(
 			new FormGroup({
-				'name': new FormControl(),
-				'amount': new FormControl()
+				'name': new FormControl(null, Validators.required),
+				'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
 			})
-		)
+		);
 	}
 
 	// REACTIVE FORM APPROACH
@@ -44,7 +57,7 @@ export class RecipeEditComponent implements OnInit {
 		let recipeName = '';
 		let recipeImagePath = '';
 		let recipeDescription = '';
-		let recipeIngredients = new FormArray([]);
+		const recipeIngredients = new FormArray([]);
 
 		if (this.editMode) {
 			const recipe = this.recipeService.getRecipe(this.id);
@@ -54,17 +67,17 @@ export class RecipeEditComponent implements OnInit {
 			if (recipe.ingredients) {
 				for (const ingredient of recipe.ingredients) {
 					recipeIngredients.push(new FormGroup({
-						'name': new FormControl(ingredient.name),
-						'amount': new FormControl(ingredient.amount)
+						'name': new FormControl(ingredient.name, Validators.required),
+						'amount': new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
 					}));
 				}
 			}
 		}
 
 		this.recipeForm = new FormGroup({
-			'name': new FormControl(recipeName),
-			'imagePath': new FormControl(recipeImagePath),
-			'description': new FormControl(recipeDescription),
+			'name': new FormControl(recipeName, Validators.required),
+			'imagePath': new FormControl(recipeImagePath, Validators.required),
+			'description': new FormControl(recipeDescription, Validators.required),
 			'ingredients': recipeIngredients
 		});
 	}
--- Angular-Project/src/app/recipes/recipes-list/recipes-list.component.ts
+++ Angular-Project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,28 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-recipes-list',
 	templateUrl: './recipes-list.component.html',
 	styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent implements OnInit {
+export class RecipesListComponent implements OnInit, OnDestroy {
 	// tslint:disable-next-line:member-ordering
 	recipes: Recipe[];
+	subscription: Subscription;
 
 	constructor(
 		private recipeService: RecipeService,
 		private router: Router,
 		private activatedRoute: ActivatedRoute
-	) {}
+	) { }
 
 	ngOnInit() {
 		this.recipes = this.recipeService.getRecipes();
+		this.subscription = this.recipeService.recipesChanged.subscribe(
+			(recipes: Recipe[]) => (this.recipes = recipes)
+		);
 	}
 
 	onNewRecipe() {
 		this.router.navigate(['new'], { relativeTo: this.activatedRoute });
+	}
+
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
 	}
 }
