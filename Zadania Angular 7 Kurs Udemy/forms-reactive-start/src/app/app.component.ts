4b1984a5824a0c3645c085152a444fee14588766
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-25 10:47:17 +0100

    async validator

--- Zadania Angular 7 Kurs Udemy/forms-reactive-start/src/app/app.component.ts
+++ Zadania Angular 7 Kurs Udemy/forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,10 +19,10 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email, this.forbiddenEmails])
       }),
       'gender': new FormControl('male', Validators.required),
-      'hobbies':  new FormArray([])
+      'hobbies': new FormArray([])
     });
   }
 
@@ -36,11 +37,26 @@ export class AppComponent implements OnInit {
   }
 
   // custom validator
-  forbiddenNames(control: FormControl): {[s: string]: boolean} {
-    if(this.forbiddenUsernames.indexOf(control.value) !== -1) {
-      return {'nameIsForbidden': true};
+  // jesli używamy 'this' w metodzie to musimy uzyć bind(this)
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidden': true };
     }
     // jesli walidacja przeszla, trzeba zwrocic null albo nic
     return null;
+  }
+
+  // custom async validator
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'emailIsForbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
