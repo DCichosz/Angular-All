534fcd5e87ab3568f0baf6eb1f6cd2ebdbc157dc
Damian Cichosz <damian.cichosz97@gmail.com>
2019-03-25 17:42:23 +0100

    http put

--- Zadania Angular 7 Kurs Udemy/http-start/src/app/server.service.ts
+++ Zadania Angular 7 Kurs Udemy/http-start/src/app/server.service.ts
@@ -6,12 +6,13 @@ export class ServerService {
   constructor(private httpService: Http) { }
 
   storeServers(servers: any[]) {
-
     // można przesyłać właśne headery
     const headers = new Headers({'Content-Type': 'application/json'});
-
     // zwróci to tylko observable -> zeby wyslac polecenie musimy zasubskrybować
-    return this.httpService.post('https://udemy-ng-http-92fec.firebaseio.com/data.json', servers, {headers: headers});
+     return this.httpService.post('https://udemy-ng-http-92fec.firebaseio.com/data.json', servers, {headers: headers});
+
+
+   // return this.httpService.put('https://udemy-ng-http-92fec.firebaseio.com/data.json', servers, {headers: headers});
   }
 
   getServers() {
