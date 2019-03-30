bf87b7ee723d3c199c7daa3729f88ada60791e21
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-03-30 17:19:32 +0100

    creating first module - recipe's module

--- Angular-Project/src/app/app.module.ts
+++ Angular-Project/src/app/app.module.ts
@@ -1,40 +1,34 @@
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { AppRoutingModule } from './app.routing.module';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeaderComponent,
-		RecipesComponent,
-		RecipesListComponent,
-		RecipeItemComponent,
 		ShoppingListComponent,
 		ShoppingEditComponent,
-		RecipeDetailComponent,
 		DropdownDirective,
-		RecipeStartComponent,
-		RecipeEditComponent,
 		SigninComponent,
 		SignupComponent
 	],
-	imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpModule, AppRoutingModule],
+	imports: [BrowserModule,
+		FormsModule,
+		HttpModule,
+		AppRoutingModule,
+		RecipesModule
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
---
+++ Angular-Project/src/app/recipes/recipes.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { DropdownDirective } from '../shared/dropdown.directive';
+import { CommonModule } from '@angular/common';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes-list/recipe-item/recipe-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app.routing.module';
+
+
+@NgModule({
+	declarations: [
+		RecipesComponent,
+		RecipeStartComponent,
+		RecipesListComponent,
+		RecipeItemComponent,
+		RecipeEditComponent,
+		RecipeDetailComponent
+	],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule
+	]
+})
+export class RecipesModule { }
