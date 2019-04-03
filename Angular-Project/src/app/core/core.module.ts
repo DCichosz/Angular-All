cd9dc38fe1d7410e4a21e46973c1d0b4032ab6ed
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-04-03 07:33:57 +0200

    interceptors

--- Angular-Project/src/app/core/core.module.ts
+++ Angular-Project/src/app/core/core.module.ts
@@ -1,5 +1,8 @@
-import { AppRoutingModule } from './../app.routing.module';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthInterceptor } from './../shared/auth.interceptor';
+import { AppRoutingModule } from './../app.routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -7,6 +10,10 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
 	declarations: [HeaderComponent, HomeComponent],
 	imports: [SharedModule, AppRoutingModule],
-	exports: [AppRoutingModule, HeaderComponent, HomeComponent]
+	exports: [AppRoutingModule, HeaderComponent, HomeComponent],
+	// dodanie interceptora
+	providers: [
+		{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+	]
 })
 export class CoreModule {}
---
+++ Angular-Project/src/app/shared/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import {
+	HttpInterceptor,
+	HttpRequest,
+	HttpHandler,
+	HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export class AuthInterceptor implements HttpInterceptor {
+	intercept(
+		req: HttpRequest<any>,
+		next: HttpHandler
+	): Observable<HttpEvent<any>> {
+		console.log('Intercepted! ', req);
+
+		// zawsze trzeba wywołac, next.handle(req) -> to daję, że request się wykona
+		return next.handle(req);
+	}
+}
