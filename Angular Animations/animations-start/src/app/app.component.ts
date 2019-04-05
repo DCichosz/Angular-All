241d5333f6a59cf21d8a690f2ac5c58f541431c9
Damian Cichosz <damian.cichosz97@gmail.com>
2019-04-05 19:34:38 +0200

    using animation callbacks

--- Angular Animations/animations-start/src/app/app.component.ts
+++ Angular Animations/animations-start/src/app/app.component.ts
@@ -128,4 +128,12 @@ export class AppComponent {
   onDelete(item) {
     this.list.splice(this.list.indexOf(item), 1);
   }
+
+  animationStarted(event) {
+    console.log(event);
+  }
+
+  animationEnded(event) {
+    console.log(event);
+  }
 }
