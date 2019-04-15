6c63ba36d163a41ae5277709b08b8fc281e48fe5
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-04-15 15:20:14 +0200

    added matformfieldmodule

--- Angular Custom Project Kurs/Fitness-Tracker/src/app/material.module.ts
+++ Angular Custom Project Kurs/Fitness-Tracker/src/app/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule]
 })
 export class MaterialModule {}
