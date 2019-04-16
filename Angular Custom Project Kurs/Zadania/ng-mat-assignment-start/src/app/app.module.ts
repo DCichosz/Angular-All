96fd269607d1c5470dbb63f1bb40c3e7a7dc6eb2
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-04-16 11:38:02 +0200

    angular material assignment finish - reactive approach

--- Angular Custom Project Kurs/Zadania/ng-mat-assignment-start/src/app/app.module.ts
+++ Angular Custom Project Kurs/Zadania/ng-mat-assignment-start/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -33,7 +33,8 @@ import { AppRoutingModule } from './app-routing.module';
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular Custom Project Kurs/Zadania/ng-mat-assignment-start/src/app/auth/login/login.component.ts
+++ Angular Custom Project Kurs/Zadania/ng-mat-assignment-start/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email]
+      }),
+      password: new FormControl('', { validators: [Validators.required] })
+    });
   }
 
+  onSubmit() {
+    console.log(this.loginForm);
+  }
 }
