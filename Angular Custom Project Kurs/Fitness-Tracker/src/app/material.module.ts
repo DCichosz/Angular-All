87594a69d5f8b1c62ae85f2577b5c86dd4d5aeb2
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-04-17 10:36:35 +0200

    added tabs for trainings

--- Angular Custom Project Kurs/Fitness-Tracker/src/app/material.module.ts
+++ Angular Custom Project Kurs/Fitness-Tracker/src/app/material.module.ts
@@ -9,7 +9,8 @@ import {
   MatCheckboxModule,
   MatSidenavModule,
   MatToolbarModule,
-  MatListModule
+  MatListModule,
+  MatTabsModule
 } from '@angular/material';
 
 @NgModule({
@@ -23,7 +24,8 @@ import {
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule
   ],
   exports: [
     MatButtonModule,
@@ -35,7 +37,8 @@ import {
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule
   ]
 })
 export class MaterialModule {}
