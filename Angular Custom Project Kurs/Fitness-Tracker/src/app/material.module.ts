10d82d2c09a928177acb07b9f9d7585240202a30
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-04-17 11:24:23 +0200

    added dropdown menu for new trainings

--- Angular Custom Project Kurs/Fitness-Tracker/src/app/material.module.ts
+++ Angular Custom Project Kurs/Fitness-Tracker/src/app/material.module.ts
@@ -11,7 +11,8 @@ import {
   MatToolbarModule,
   MatListModule,
   MatTabsModule,
-  MatCardModule
+  MatCardModule,
+  MatSelectModule
 } from '@angular/material';
 
 @NgModule({
@@ -27,7 +28,8 @@ import {
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule
   ],
   exports: [
     MatButtonModule,
@@ -41,7 +43,8 @@ import {
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule {}
