6e98f03bce3604f9b3547999028d6d6d41645939
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-04-17 12:57:51 +0200

    added possibility to pass data to the dialog

--- Angular Custom Project Kurs/Fitness-Tracker/src/app/training/current-training/current-training.component.ts
+++ Angular Custom Project Kurs/Fitness-Tracker/src/app/training/current-training/current-training.component.ts
@@ -24,6 +24,12 @@ export class CurrentTrainingComponent implements OnInit {
 
   onStop() {
     clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: { progress: this.progress }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
   }
 }
--- Angular Custom Project Kurs/Fitness-Tracker/src/app/training/current-training/stop-training.component.ts
+++ Angular Custom Project Kurs/Fitness-Tracker/src/app/training/current-training/stop-training.component.ts
@@ -1,11 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-stop-training',
-  template: `<h1 mat-dialog-title>Are you sure?</h1>
-              <mat-dialog-actions>
-                  <button mat-button [mat-dialog-close]="true">Yes</button>
-                  <button mat-button [mat-dialog-close]="false">No</button>
-              </mat-dialog-actions>`
+  template: `
+    <h1 mat-dialog-title>Are you sure?</h1>
+    <p>You already got {{ passedData.progress }}%</p>
+    <mat-dialog-content></mat-dialog-content>
+    <mat-dialog-actions>
+      <button mat-button [mat-dialog-close]="true">Yes</button>
+      <button mat-button [mat-dialog-close]="false">No</button>
+    </mat-dialog-actions>
+  `
 })
-export class StopTrainingComponent {}
+export class StopTrainingComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {}
+}
