31812a63392b16fbb8e7bf77b8da81dd6ce771aa
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-04-17 13:03:28 +0200

    added 'exit' and 'continue'  training options

--- Angular Custom Project Kurs/Fitness-Tracker/src/app/training/current-training/current-training.component.ts
+++ Angular Custom Project Kurs/Fitness-Tracker/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training.component';
 
@@ -8,12 +8,17 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter<void>();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
+    this.startOrResumeTraining();
+  }
+
+  startOrResumeTraining() {
     this.timer = setInterval(() => {
       this.progress += 5;
       if (this.progress >= 100) {
@@ -29,7 +34,11 @@ export class CurrentTrainingComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTraining();
+      }
     });
   }
 }
