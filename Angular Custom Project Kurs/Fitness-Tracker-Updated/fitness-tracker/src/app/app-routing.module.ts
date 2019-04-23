5bbbadcd823f25a96281bd479de42a0f48e51be6
Damian Cichosz <damian.cichosz@si-consulting.pl>
2019-04-23 09:28:18 +0200

    route protection for training

--- Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/app/app-routing.module.ts
+++ Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/app/app-routing.module.ts
@@ -5,16 +5,19 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: WelcomeComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent }
+	{path: '', component: WelcomeComponent},
+	{path: 'signup', component: SignupComponent},
+	{path: 'login', component: LoginComponent},
+	{path: 'training', component: TrainingComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+	providers: [AuthGuard]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
