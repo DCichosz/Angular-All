ff9a9debebd207a28e1ff6f7fe95714c725a0771
Damian Cichosz <damian.cichosz97@gmail.com>
2019-04-25 22:05:07 +0200

    moved the authguard to app-routing.module and enabled canLoad method for lazyloading

--- Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/app/auth/auth.guard.ts
+++ Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/app/auth/auth.guard.ts
@@ -1,17 +1,32 @@
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+	ActivatedRouteSnapshot,
+	CanActivate,
+	CanLoad, Route,
+	Router,
+	RouterStateSnapshot,
+	UrlSegment,
+	UrlTree
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
 	constructor(private authService: AuthService, private router: Router) {
 	}
 
-	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+		if (this.authService.isAuth()) {
+			return true;
+		} else {
+			this.router.navigate(['/login']);
+		}
+	}
 
+	canLoad(route: Route) {
 		if (this.authService.isAuth()) {
 			return true;
 		} else {
--- Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/app/training/training-routing.module.ts
+++ Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/app/training/training-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { TrainingComponent } from './training.component';
-import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
-	{path: '', component: TrainingComponent, canActivate: [AuthGuard]}
+	{path: '', component: TrainingComponent}
 ];
 
 @NgModule({
