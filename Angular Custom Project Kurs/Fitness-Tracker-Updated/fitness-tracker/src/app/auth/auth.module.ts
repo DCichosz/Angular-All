32bee49042d8e99f053386d27a06b153ccb74e1b
Damian Cichosz <damian.cichosz97@gmail.com>
2019-04-25 21:32:58 +0200

    added auth-routing module

--- Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/app/auth/auth.module.ts
+++ Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/app/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { environment } from '../../environments/environment';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
 	declarations: [
@@ -16,6 +17,7 @@ import { SharedModule } from '../shared/shared.module';
 		ReactiveFormsModule,
 		AngularFireModule.initializeApp(environment.firebase),
 		SharedModule,
+		AuthRoutingModule
 	],
 	exports: []
 })
