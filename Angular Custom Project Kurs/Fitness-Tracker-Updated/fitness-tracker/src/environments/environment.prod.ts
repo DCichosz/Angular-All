a6789f5e01e22c2321b17b3e8b7d8470c2fb44ba
Damian Cichosz <damian.cichosz97@gmail.com>
2019-04-25 22:13:10 +0200

    added missing firebase configuration to production env

--- Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/environments/environment.prod.ts
+++ Angular Custom Project Kurs/Fitness-Tracker-Updated/fitness-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+	firebase: {
+		apiKey: 'AIzaSyCNyiWt07qKHmJgLkyWCp1bIBTFcmiyLBg',
+		authDomain: 'ng-fitness-tracker-eed81.firebaseapp.com',
+		databaseURL: 'https://ng-fitness-tracker-eed81.firebaseio.com',
+		projectId: 'ng-fitness-tracker-eed81',
+		storageBucket: 'ng-fitness-tracker-eed81.appspot.com',
+		messagingSenderId: '268091878666'
+	}
 };
